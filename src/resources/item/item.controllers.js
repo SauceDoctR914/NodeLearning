8eb79926824312fae49698d04c7a9113c2e9317e
GavIn Sokolof <sokolofg@gmail.com>
2019-04-10 23:01:46 -0400

    connect function in item.controllers not hitting

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,4 +1,22 @@
-import { crudControllers } from '../../utils/crud'
 import { Item } from './item.model'
 
+import { crudControllers } from '../../utils/crud'
+
+import { connect } from '../../utils/db'
+
+// const createOne = model.create()
+
+//C model.create new model()
+//R model.find() model.findOne() model.findById()
+
+const run = async () => {
+  await connect()
+  const item = Item.create({
+    name: 'first Item',
+    createdBy: mongoose.Types.ObjectId(),
+    list: mongoose.Types.ObjectId()
+  })
+  console.log(item, 'Hi')
+}
+run()
 export default crudControllers(Item)
