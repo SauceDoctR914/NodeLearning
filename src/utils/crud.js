89f1d36df935dbb3f1dd38f66c423706bbfba798
GavIn Sokolof <sokolofg@gmail.com>
2019-04-11 23:25:29 -0400

    passing 6

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -8,7 +8,7 @@ export const getOne = model => async (req, res) => {
       .lean()
       .exec()
     if (!doc) {
-      res.status(400).end()
+      return res.status(404).end()
     }
     res.status(200).json({ data: doc })
   } catch (e) {
@@ -16,6 +16,7 @@ export const getOne = model => async (req, res) => {
     res.status(400).end()
   }
 }
+
 export const getMany = model => async (req, res) => {
   try {
     const docs = await model
@@ -33,11 +34,8 @@ export const getMany = model => async (req, res) => {
 export const createOne = model => async (req, res) => {
   const createdBy = req.user._id
   try {
-    const doc = await model
-      .create({ ...req.body, createdBy })
-      .lean()
-      .exec()
-    res.status(200).json({ data: doc })
+    const doc = await model.create({ ...req.body, createdBy })
+    res.status(201).json({ data: doc })
   } catch (e) {
     console.error(e)
     res.status(400).end()
@@ -69,39 +67,6 @@ export const updateOne = model => async (req, res) => {
 
 export const removeOne = model => async (req, res) => {}
 
-// export const getOne = model => async (req, res) => {
-//   try {
-//     const doc = await model
-//       .findOne({ createdBy: req.user._id, _id: req.params.id })
-//       .lean()
-//       .exec()
-//
-//     if (!doc) {
-//       return res.status(400).end()
-//     }
-//
-//     res.status(200).json({ data: doc })
-//   } catch (e) {
-//     console.error(e)
-//     res.status(400).end()
-//   }
-// }
-//
-// export const getMany = model => async (req, res) => {
-//   console.log('hello')
-//   try {
-//     const docs = await model
-//       .find({ createdBy: req.user._id })
-//       .lean()
-//       .exec()
-//
-//     res.status(200).json({ data: docs })
-//   } catch (e) {
-//     console.error(e)
-//     res.status(400).end()
-//   }
-// }
-//
 // export const createOne = model => async (req, res) => {
 //   const createdBy = req.user._id
 //   try {
